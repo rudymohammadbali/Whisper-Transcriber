29c4df138d9d77c878820b1287ea85a2d1275b03
Youssef Essalhi <76540840+YoussefEssalhi@users.noreply.github.com>
2024-02-28 03:16:08 +0100

    Update Gpu-Details ⚙️
    - Fix The Settings UI .

--- src/logic/gpu_details.py
+++ src/logic/gpu_details.py
@@ -10,7 +10,7 @@ class GPUInfo:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.cuda_available = torch.cuda.is_available()
         current_dir = os.path.dirname(__file__)
-        file_path = os.path.join(current_dir, '..', '..', f'assets{os.path.sep}config')
+        file_path = os.path.join(current_dir, '..', '..', f"assets{os.path.sep}config")
         self.config_path = os.path.normpath(file_path)
         self.gpu_info_file_path = os.path.join(self.config_path, "gpu_info.json")
 
@@ -18,35 +18,27 @@ class GPUInfo:
         if self.cuda_available:
             gpu_count = torch.cuda.device_count()
             current_device = torch.cuda.current_device()
-            device_name = torch.cuda.get_device_name(current_device)
+            gpu_name = torch.cuda.get_device_name(current_device)
             total_memory = round(torch.cuda.get_device_properties(current_device).total_memory / (1024 ** 3))
-            settings = {
-                "cuda_available": self.cuda_available,
-                "gpu_count": gpu_count,
-                "current_gpu": current_device,
-                "gpu_name": device_name,
-                "total_memory": total_memory,
-            }
-            return settings
         else:
             gpus = GPUtil.getGPUs()
             gpu_count = len(gpus)
 
             if gpu_count == 0:
-                return "No GPU found."
+                current_device = "No GPU found."
+                gpu_name = "N/A"
+                total_memory = 0
+            else:
+                current_gpu = gpus[0]
+                current_device = f"{current_gpu.name} ({current_gpu.id})"
+                gpu_name = current_gpu.name
+                total_memory = current_gpu.memoryTotal / 1024
 
-            current_gpu = gpus[0]
-            current_gpu_str = f"{current_gpu.name} ({current_gpu.id})"
-            gpu_name = current_gpu.name
-            total_memory_gb = current_gpu.memoryTotal / 1024
-
-            return {
-                "cuda_available": self.cuda_available,
-                "gpu_count": gpu_count,
-                "current_gpu": current_gpu_str,
-                "gpu_name": gpu_name,
-                "total_memory": int(total_memory_gb)
-            }
+        return {"cuda_available": self.cuda_available,
+                "gpu_count"     : gpu_count,
+                "current_gpu"   : current_device,
+                "gpu_name"      : gpu_name,
+                "total_memory"  : total_memory}
 
     def save_gpu_info(self):
         settings = self.get_gpu_info()
@@ -55,22 +47,16 @@ class GPUInfo:
             if not os.path.exists(self.config_path):
                 os.mkdir(self.config_path)
 
-            with open(self.gpu_info_file_path, 'w') as file:
-                json.dump(settings, file)
-        except FileNotFoundError:
-            pass
-        except PermissionError:
+            with open(self.gpu_info_file_path, 'w') as file: json.dump(settings, file)
+        except FileNotFoundError or PermissionError:
             pass
 
     def load_gpu_info(self):
+        if not os.path.exists(self.gpu_info_file_path) or os.path.getsize(self.gpu_info_file_path) == 0:
+            self.save_gpu_info()
         try:
-            if not os.path.exists(self.gpu_info_file_path) or os.path.getsize(self.gpu_info_file_path) == 0:
-                self.save_gpu_info()
-            with open(self.gpu_info_file_path, "r") as file:
-                loaded_settings = json.load(file)
-            return loaded_settings
+            with open(self.gpu_info_file_path, 'r') as file: loaded_settings = json.load(file)
         except FileNotFoundError:
             self.save_gpu_info()
-            with open(self.gpu_info_file_path, "r") as file:
-                loaded_settings = json.load(file)
-            return loaded_settings
+            with open(self.gpu_info_file_path, 'r') as file: loaded_settings = json.load(file)
+        return loaded_settings
